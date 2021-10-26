9b63f155e6eda82855265b40a92c17eb434f9766
Niño Mollaneda <ninoo.mollaneda@gmail.com>
2021-10-26 19:43:25 +0800

    Add solution till exercise 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          backgroundColor: "hsl(195deg, 20%, 86%)",
+          height: "40vh",
+          position: "fixed",
+          bottom: 0,
+          width: "100%",
+          // zIndex: -1,
+        }}
+      />
       <CharacterEditor />
       <Footer />
     </>
